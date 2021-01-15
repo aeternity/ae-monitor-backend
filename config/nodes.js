190be15d64fea1f9cb7835bc0add815bdf2a9405
kenodressel <hire@keno.digital>
2021-01-15 15:58:22 +0100

    adds two new servers to the config

--- config/nodes.js
+++ config/nodes.js
@@ -1,6 +1,14 @@
 module.exports = [
   {
     nodeId: 'fra',
-    url: 'https://mainnet-explorer.aeternity.art/v2/debug/network'
+    url: 'http://monitor.aeternity.art:3113/v2/debug/network'
+  },
+  {
+    nodeId: 'sfo',
+    url: 'https://138.68.22.27:3113/v2/debug/network'
+  },
+  {
+    nodeId: 'sgp',
+    url: 'http://128.199.108.118:3113/v2/debug/network'
   }
 ]
